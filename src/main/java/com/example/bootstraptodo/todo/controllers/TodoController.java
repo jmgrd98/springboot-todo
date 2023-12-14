b1dfc5a36d698d7d21ed8bc53c01f0b59aed396d
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-14 09:40:58 -0300

    Update TodoController.java

--- src/main/java/com/example/bootstraptodo/todo/controllers/TodoController.java
+++ src/main/java/com/example/bootstraptodo/todo/controllers/TodoController.java
@@ -1,17 +1,44 @@
 package com.example.bootstraptodo.todo.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.bootstraptodo.todo.models.Todo;
+import com.example.bootstraptodo.todo.services.TodoService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
 
+    @Autowired
+    private TodoService todoService;
+
     @GetMapping
-    public String getTodos() {
-        return "hello world";
+    public ResponseEntity<List<Todo>> getAllTodos() {
+        List<Todo> todos = todoService.allTodos();
+        return new ResponseEntity<List<Todo>>(todos, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable("Id"), Long id) {
+        Todo todo = todoService.findTodoById(id);
+        return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public Response<Todo> addTodo(@RequestBody Todo todo) {
+        Todo newTodo = todoService.addTodo(todo);
+        return new ResponseEntity<Todo>(newTodo, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateTodo(@PathVariable("id") Long id) {
+        todoService.deleteTodo(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
