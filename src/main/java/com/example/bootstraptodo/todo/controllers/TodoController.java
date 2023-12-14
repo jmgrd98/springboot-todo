c686a14859d69c2ba3e76799626381f8bab05932
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-14 07:51:11 -0300

    todo controller and repository

---
+++ src/main/java/com/example/bootstraptodo/todo/controllers/TodoController.java
@@ -0,0 +1,18 @@
+package com.example.bootstraptodo.todo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @GetMapping
+    public String getTodos() {
+        return "hello world";
+    }
+
+
+}
